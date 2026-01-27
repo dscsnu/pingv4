71842b68ef3d20cfafe20bafa5386a65747c7879
lalitm1004 <lalitm1004@gmail.com>
2026-01-27 16:52:42 +0000

    feat: switch to column major storage

--- src/core/game/board.rs
+++ src/core/game/board.rs
@@ -12,8 +12,9 @@ pub enum TurnResult<const R: usize, const C: usize> {
 
 #[derive(Debug, Clone)]
 pub struct Board<const R: usize, const C: usize, S: GameState> {
-    cell_states: [[Option<CellState>; R]; C],
+    cell_states: [[Option<CellState>; C]; R],
     game_state: S,
+    hash: u64,
 }
 
 impl<const R: usize, const C: usize, S: GameState> Board<R, C, S> {
@@ -23,7 +24,18 @@ impl<const R: usize, const C: usize, S: GameState> Board<R, C, S> {
     }
 
     #[inline]
-    pub const fn cell_states(&self) -> &[[Option<CellState>; R]; C] {
+    pub const fn cell_states(&self) -> &[[Option<CellState>; C]; R] {
         &self.cell_states
+    }
+
+    pub fn get_column_hash(&self, col_idx: usize) -> u64 {
+        assert!(col_idx < C, "column index {} out of bounds", col_idx);
+
+        let column = &self.cell_states[col_idx];
+        todo!()
+    }
+
+    pub fn get_board_hash() -> u64 {
+        todo!()
     }
 }
