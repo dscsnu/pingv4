88b3293cea72142e26011f1ed90e6003e99bc41e
DJ-22 <jaindaksh006@gmail.com>
2026-01-29 12:00:10 +0530

    feat(core): implement perfect hash functions for board encoding

--- src/core/game/board.rs
+++ src/core/game/board.rs
@@ -14,7 +14,7 @@ pub enum TurnResult<const R: usize, const C: usize> {
 pub struct Board<const R: usize, const C: usize, S: GameState> {
     cell_states: [[Option<CellState>; C]; R],
     game_state: S,
-    hash: u64,
+    cached_hash: u64,
 }
 
 impl<const R: usize, const C: usize, S: GameState> Board<R, C, S> {
@@ -28,14 +28,74 @@ impl<const R: usize, const C: usize, S: GameState> Board<R, C, S> {
         &self.cell_states
     }
 
+    #[inline]
+    pub fn hash(&self) -> u64 {
+        self.cached_hash
+    }
+
+    #[inline]
+    pub fn update_hash(&mut self) {
+        self.cached_hash = self.get_board_hash();
+    }
+
     pub fn get_column_hash(&self, col_idx: usize) -> u64 {
         assert!(col_idx < C, "column index {} out of bounds", col_idx);
 
-        let column = &self.cell_states[col_idx];
-        todo!()
+        let mut hash = 0u64;
+        
+        for (pos, row_idx) in (0..R).rev().enumerate() {
+            if let Some(cell_state) = self.cell_states[row_idx][col_idx] {
+                let bit = match cell_state {
+                    CellState::Yellow => 0,
+                    CellState::Red => 1,
+                };
+                hash += bit << pos;
+            } else {
+                break;
+            }
+        }
+
+        hash
     }
 
-    pub fn get_board_hash() -> u64 {
-        todo!()
+    fn get_column_height(&self, col_idx: usize) -> usize {
+        assert!(col_idx < C, "column index {} out of bounds", col_idx);
+
+        let mut height = 0;
+        for row_idx in (0..R).rev() {
+            if self.cell_states[row_idx][col_idx].is_some() {
+                height += 1;
+            } else {
+                break;
+            }
+        }
+
+        height
+    }
+
+    pub fn get_board_hash(&self) -> u64 {
+        let mut columns: Vec<(usize, u64, usize)> = (0..C)
+            .map(|col_idx| {
+                let height = self.get_column_height(col_idx);
+                let col_hash = self.get_column_hash(col_idx);
+                (height, col_hash, col_idx)
+            })
+            .collect();
+
+        columns.sort_by_key(|&(height, col_hash, _)| (height, col_hash));
+
+        let mut board_hash = 0u64;
+
+        for (height, col_hash, _) in columns {
+            let offset = if height > 0 {
+                (1u64 << height) - 1
+            } else {
+                0
+            };
+
+            board_hash += offset + col_hash;
+        }
+
+        board_hash
     }
 }
